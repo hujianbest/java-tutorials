5a1d7f1cc2ae9c79c5bbc48f4cd52f97f83cdb81
hujianbest <jianbesthu@163.com>
2019-12-30 00:00:45 +0800

    [hj] try var

---
+++ jdk13/src/main/java/org/hujianbest/jdk13/Main.java
@@ -0,0 +1,11 @@
+package org.hujianbest.jdk13;
+
+/**
+ * @author hujian
+ */
+public class Main {
+    public static void main(String[] args) {
+        var i = 10;
+        System.out.println(i);
+    }
+}
