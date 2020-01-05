666a8a9bbba69eb1161ec1414180f861ed766b5d
hujianbest <jianbesthu@163.com>
2020-01-05 21:35:10 +0800

    [hj] add 函数复合

--- function/src/main/java/org/hujianbest/function/Main.java
+++ function/src/main/java/org/hujianbest/function/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(test("Hello", (String s) -> s + " World! "));
-        System.out.println(test("Hello", s -> s + " World! "));
+        System.out.println(test("Hello", ((MyFunction<String, String>) s1 -> s1 + " World! ").andThen(s -> s + " ni hao!")));
 
         MyObject object = getObject(MyObject::new);
         object.say();
--- function/src/main/java/org/hujianbest/function/MyFunction.java
+++ function/src/main/java/org/hujianbest/function/MyFunction.java
@@ -1,9 +1,17 @@
 package org.hujianbest.function;
 
+import java.util.Objects;
+import java.util.function.Function;
+
 /**
  * @author hujian
  */
 @FunctionalInterface
 public interface MyFunction<T, R> {
     R apply(T t);
+
+    default <V> MyFunction<T, V> andThen(Function<? super R, ? extends V> after) {
+        Objects.requireNonNull(after);
+        return (T t) -> after.apply(apply(t));
+    }
 }
