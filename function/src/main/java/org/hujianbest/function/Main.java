d5cbd6fe52926acd1b650e3ff9afb9f10a979225
hujianbest <jianbesthu@163.com>
2020-01-05 20:54:15 +0800

    [hj] add supplier interface

--- function/src/main/java/org/hujianbest/function/Main.java
+++ function/src/main/java/org/hujianbest/function/Main.java
@@ -1,5 +1,7 @@
 package org.hujianbest.function;
 
+import java.util.function.Supplier;
+
 /**
  * @author hujian
  */
@@ -8,8 +10,15 @@ public class Main {
         return function.apply(string);
     }
 
+    public static <T> T getObject(Supplier<T> constructor) {
+        return constructor.get();
+    }
+
     public static void main(String[] args) {
         System.out.println(test("Hello", (String s) -> s + " World! "));
         System.out.println(test("Hello", s -> s + " World! "));
+
+        MyObject object = getObject(MyObject::new);
+        object.say();
     }
 }
---
+++ function/src/main/java/org/hujianbest/function/MyObject.java
@@ -0,0 +1,10 @@
+package org.hujianbest.function;
+
+/**
+ * @author hujian
+ */
+public class MyObject {
+    public void say(){
+        System.out.println("Hello World!");
+    }
+}
