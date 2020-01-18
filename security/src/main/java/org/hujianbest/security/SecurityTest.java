6820a2d05783f5a578a3f97186a4cf999f1a2326
hujianbest <jianbesthu@163.com>
2020-01-18 22:40:27 +0800

    [hj] java安全管理

---
+++ security/src/main/java/org/hujianbest/security/SecurityTest.java
@@ -0,0 +1,11 @@
+package org.hujianbest.security;
+
+/**
+ * @author hujian
+ */
+public class SecurityTest {
+    public static void main(String[] args) {
+        SecurityManager securityManager = System.getSecurityManager();
+        System.out.println(securityManager);
+    }
+}
