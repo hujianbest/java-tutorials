de645d9d4a7e3695876b0cdddbc0abb6a9e2d5f8
hujianbest <jianbesthu@163.com>
2020-01-18 18:21:06 +0800

    [hj] socket init

---
+++ network/src/main/java/org/hujianbest/network/SocketTest.java
@@ -0,0 +1,20 @@
+package org.hujianbest.network;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @author hujian
+ */
+public class SocketTest {
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("time-a.nist.gov", 13);
+             Scanner in = new Scanner(socket.getInputStream(), "UTF-8")) {
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                System.out.println(line);
+            }
+        }
+    }
+}
