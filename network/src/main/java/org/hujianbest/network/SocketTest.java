b7ad0c354922949f4619e52ecd1b4d10fdd2ad33
hujianbest <jianbesthu@163.com>
2020-01-18 18:57:09 +0800

    [hj] socket server

--- network/src/main/java/org/hujianbest/network/SocketTest.java
+++ network/src/main/java/org/hujianbest/network/SocketTest.java
@@ -24,8 +24,15 @@ public class SocketTest {
         InetAddress addressIp = InetAddress.getByName("time-a.nist.gov");
         byte[] addressBytes = addressIp.getAddress();
         System.out.println(Arrays.toString(addressBytes));
+        connect(address);
+
+        InetSocketAddress localhost = new InetSocketAddress("localhost", 8080);
+        connect(localhost);
+    }
+
+    private static void connect(InetSocketAddress localhost) throws IOException {
         try (Socket socket = new Socket()) {
-            socket.connect(address, 10000);
+            socket.connect(localhost, 10000);
             socket.setSoTimeout(10000);
             try (Scanner in = new Scanner(socket.getInputStream(), "UTF-8")) {
                 while (in.hasNextLine()) {
