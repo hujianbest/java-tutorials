0fd92027838c361aad4bf9083635290356c62aec
hujianbest <jianbesthu@163.com>
2020-01-18 18:28:53 +0800

    [hj] socket 设置超时时间

--- network/src/main/java/org/hujianbest/network/SocketTest.java
+++ network/src/main/java/org/hujianbest/network/SocketTest.java
@@ -1,6 +1,7 @@
 package org.hujianbest.network;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -14,6 +15,19 @@ public class SocketTest {
             while (in.hasNextLine()) {
                 String line = in.nextLine();
                 System.out.println(line);
+            }
+        }
+
+        InetSocketAddress address = new InetSocketAddress("time-a.nist.gov", 13);
+        try (Socket socket = new Socket()) {
+            socket.connect(address, 10000);
+            socket.setSoTimeout(10000);
+            try (Scanner in = new Scanner(socket.getInputStream(), "UTF-8")) {
+                while (in.hasNextLine()) {
+                    String line = in.nextLine();
+                    System.out.println(line);
+                }
+
             }
         }
     }
